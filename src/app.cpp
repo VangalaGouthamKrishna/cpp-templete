fc2b59ce9ca3622cd6bb835482712769d43c4870
VangalaGouthamKrishna <69141511+VangalaGouthamKrishna@users.noreply.github.com>
2021-10-12 11:05:07 +0200

    cpp templete

--- src/app.cpp
+++ src/app.cpp
@@ -1,13 +1,77 @@
 #include <mosaiqpp/app.hpp>
 #include <mosaiqpp/intercom/publication.hpp>
+#include <mosaiqpp/intercom/subscription.hpp>
 
 #include <iomanip>
 #include <sstream>
 #include <string>
-#include <vector>
-#include <cstddef>
-#include <algorithm>
 
+/// The class "YourClass" must publicly derive from the provided MosaiqSdk::CyclicApp
+/// It may contain members to provide Real Time Intercommunication functionality and
+/// it may also contain other members to be used for custom processing. The functions
+/// YourClass::onInitialization(), YourClass::onCyclicExecution() and YourClass::onExit()
+/// will be considered for the main functionality of the Mosaiq Runtime and must be publicly
+/// accessible.
+class YourClass : public MosaiqSdk::CyclicApp
+{
+public:
+  /// The constructor of YourClass must move the broker to its base class. You can use the broker
+  /// via MosaiqSdk::CyclicApp::getBroker() and then initialize publication and subscription members
+  /// with the handles that are returned from MosaiqSdk::Intercom::Broker::publish() and
+  /// MosaiqSdk::Intercom::Broker::subscribe() functions. Custom members can also be initialized here.
+  YourClass(MosaiqSdk::Intercom::Broker &&broker)
+      : MosaiqSdk::CyclicApp{std::move(broker)}, m_integralPub{getBroker().template publish<uint32_t>("cycleCounter")}, m_cycleCount{0}
+      // string "cycleCounter" is the topic name which is required to publish or subscribe that particular 
+      // datatype's data and to create a web socket stream
+      // NOTE: topic name should be unique across the cyclic apps in that machine and
+      // topic name cannot be an empty string
+      // NOTE: To subscribe a particular topic, then the topic name and data type of the publisher app should match
+      // Possible datatypes are:
+      // "int8_t", "uint8_t", "int16_t", "uint16_t", "int32_t", "uint32_t", "int64_t", "uint64_t",
+      // "float", "double", "std::string", "std::vector<std::byte>"
+  {
+  }
+  /// You can run procedures inside here, that need to be run once before the cyclic execution.
+  /// This step is optional.
+  void onInitialization()
+  {
+    m_cycleCount = 12; // set the value m_cycleCount to 12 before doing the cyclic execution.
+  }
+  /// The procedure inside this function is periodically called according to a preconfigured cycle time.
+  /// You can do any calculations and processing based on members of YourClass or other
+  /// variables and values here.
+  void onCyclicExecution()
+  {
+    ++m_cycleCount;
+    m_integralPub.write(m_cycleCount);
+    // at this point the value of m_cycleCount is written to the topic of m_integralPub (which is
+    // cycleCounter) and other apps that subscribed to this topic can read the updated value
+  }
+  /// You can run procedures inside here, that need to be run once after finishing the cyclic execution. 
+  /// This step is optional.
+  void onExit()
+  {
+  }
+private:
+  uint32_t m_cycleCount;
+  /// This is a member to publish integral values of the type uint32_t through the Mosaiq Runtime
+  /// Real Time Intercommunication. When the class YourClass is instantiated, this member needs to
+  /// be initialized by passing a publication handle of the type MosaiqIntercomPublication*. This
+  /// handle can be acquired via a publish through the broker, that is used as a parameter during
+  /// construction of "YourClass". MosaiqSdk::CyclicApp::getBroker() provides the broker and it can
+  /// then be used to publish by also specifying the type and topic name.
+  /// Possible datatypes are:
+  /// "int8_t", "uint8_t", "int16_t", "uint16_t", "int32_t", "uint32_t", "int64_t", "uint64_t",
+  /// "float", "double", "std::string", "std::vector<std::byte>"
+  MosaiqSdk::Intercom::Publication<uint32_t> m_integralPub;
+};
+/// This macro needs to be called for YourClass to function in the Mosaiq Runtime
+MOSAIQ_DECLARE_APP(YourClass);
+
+
+/************************************ sample publisher app templete ************************************/
+
+/* 
 
 class PublisherApp : public MosaiqSdk::CyclicApp
 {
@@ -15,60 +79,60 @@ public:
   PublisherApp(MosaiqSdk::Intercom::Broker&& broker)
     : MosaiqSdk::CyclicApp{std::move(broker)}
     , m_cycleCount{0}
-    , m_int8_t_Pub{getBroker().template publish<int8_t>("int8_t")}
-    , m_uint8_t_Pub{getBroker().template publish<uint8_t>("uint8_t")}
-    , m_int16_t_Pub{getBroker().template publish<int16_t>("int16_t")}
-    , m_uint16_t_Pub{getBroker().template publish<uint16_t>("uint16_t")}
-    , m_int32_t_Pub{getBroker().template publish<int32_t>("int32_t")}
-    , m_uint32_t_Pub{getBroker().template publish<uint32_t>("uint32_t")}
-    , m_int64_t_Pub{getBroker().template publish<int64_t>("int64_t")}
-    , m_uint64_t_Pub{getBroker().template publish<uint64_t>("uint64_t")}
-    , m_float_Pub{getBroker().template publish<float>("float")}
-    , m_double_Pub{getBroker().template publish<double>("double")}
-    , m_stringPub{getBroker().template publish<std::string>("string")}
-     , m_vectorPub{getBroker().template publish<std::vector<std::byte>>("vector")}
+    , m_integralPub{getBroker().template publish<uint32_t>("cycleCounter")}
+    , m_stringPub{getBroker().template publish<std::string>("cycleCounterHex")}
   {}
 
   void onCyclicExecution()
   {
     ++m_cycleCount;
-    m_int8_t_Pub.write(m_cycleCount);
-    m_uint8_t_Pub.write(m_cycleCount);
-    m_int16_t_Pub.write(m_cycleCount);
-    m_uint16_t_Pub.write(m_cycleCount);
-    m_int32_t_Pub.write(m_cycleCount);
-    m_uint32_t_Pub.write(m_cycleCount);
-    m_int64_t_Pub.write(m_cycleCount);
-    m_uint64_t_Pub.write(m_cycleCount);
-    m_float_Pub.write(m_cycleCount);
-    m_double_Pub.write(m_cycleCount);
+    m_integralPub.write(m_cycleCount);
 
     std::stringstream sstr;
     sstr << "0x" << std::hex << std::setfill('0') << std::setw(8) << m_cycleCount;
     m_stringPub.write(sstr.str());
-
-    auto numberAsString = sstr.str();
-    std::vector<std::byte> bytes(numberAsString.size());
-    std::transform(std::begin(numberAsString), std::end(numberAsString), std::begin(bytes), [](auto charater){
-      return static_cast<std::byte>(charater);
-    });
-    m_vectorPub.write(bytes);
   }
 
 private:
   uint32_t m_cycleCount;
-  MosaiqSdk::Intercom::Publication<int8_t> m_int8_t_Pub;
-  MosaiqSdk::Intercom::Publication<uint8_t> m_uint8_t_Pub;
-  MosaiqSdk::Intercom::Publication<int16_t> m_int16_t_Pub;
-  MosaiqSdk::Intercom::Publication<uint16_t> m_uint16_t_Pub;
-  MosaiqSdk::Intercom::Publication<int32_t> m_int32_t_Pub;
-  MosaiqSdk::Intercom::Publication<uint32_t> m_uint32_t_Pub;
-  MosaiqSdk::Intercom::Publication<int64_t> m_int64_t_Pub;
-  MosaiqSdk::Intercom::Publication<uint64_t> m_uint64_t_Pub;
-  MosaiqSdk::Intercom::Publication<float> m_float_Pub;
-  MosaiqSdk::Intercom::Publication<double> m_double_Pub;
+  MosaiqSdk::Intercom::Publication<uint32_t> m_integralPub;
   MosaiqSdk::Intercom::Publication<std::string> m_stringPub;
-  MosaiqSdk::Intercom::Publication<std::vector<std::byte>> m_vectorPub;
 };
 
 MOSAIQ_DECLARE_APP(PublisherApp);
+ 
+ */
+
+
+ /************************************ sample subscriber app templete ************************************/
+
+/* 
+
+class SubscriberApp : public MosaiqSdk::CyclicApp
+{
+public:
+  SubscriberApp(MosaiqSdk::Intercom::Broker&& broker)
+    : MosaiqSdk::CyclicApp{std::move(broker)}
+    , m_cycleCount{0}
+    , m_integralSub{getBroker().template subscribe<uint32_t>("cycleCounter")}
+    , m_stringSub{getBroker().template subscribe<std::string>("cycleCounterHex")}
+  {}
+
+  void onCyclicExecution()
+  {
+    uint32_t value{0};
+    std::string strValue{};
+    strValue.reserve(10);
+    m_integralSub.read(value);
+    m_stringSub.read(strValue);
+  }
+
+private:
+  uint32_t m_cycleCount;
+  MosaiqSdk::Intercom::Subscription<uint32_t> m_integralSub;
+  MosaiqSdk::Intercom::Subscription<std::string> m_stringSub;
+};
+
+MOSAIQ_DECLARE_APP(SubscriberApp);
+ 
+ */
